1494196e97c8cd2102a22d53b90b063c70f8fde4
Helmut Steiner <helmut.steinersti2@gmail.com>
2020-06-19 10:27:04 +0200

    error in prod config.

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,6 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :agricol, AgricolWeb.Endpoint,
-adapter: Ecto.Adapters.Postgres,
-url: {:system, "DATABASE_URL"},
-pool_size: 10
 http: [port: {:system, "PORT"}],
 url: [scheme: "https", host: "agricol.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
