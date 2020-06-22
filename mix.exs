7d045dbe4dd6e0e52f3249261efb70d24be9fa18
Helmut Steiner <helmut.steinersti2@gmail.com>
2020-06-22 10:12:59 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Agricol.MixProject do
     [
       app: :agricol,
       version: "0.1.0",
-      elixir: "~> 1.5.3",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -34,15 +34,15 @@ defmodule Agricol.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.3"},
-      {:phoenix_ecto, "~> 1.0, override: true"},   
-      {:ecto_sql, "~> 1.0, override: true"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-    #  {:gettext, "~> 0.11"},
+      {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
